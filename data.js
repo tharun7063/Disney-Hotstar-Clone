8de6b25658183bf04343b98eab89f38bc3518b60
tharun7063 <tharundharavath34@gmail.com>
2023-11-10 18:28:51 +0530

    second commit

--- data.js
+++ data.js
@@ -1,32 +1,32 @@
 let movies = [
     {
         name: 'daya',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        des: "A web of crime and politics unravels after an incident turns the life of a freezer van driver upside down. Pavan, who has written the dialogues with Rakendu Mouli, presents Dayaa's story as that of an underdog.",
         image: 'images/slider 6.jpg'
     },
     {
         name: 'loki',
-        des: ' sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        des: 'The mercurial villain Loki (Tom Hiddleston) resumes his role as the God of Mischief in a new series that takes place after the events of “Avengers: Endgame.',
         image: 'images/slider 1.PNG'
     },
     {
         name: 'wanda vision',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        des: 'Blends the style of classic sitcoms with the MCU, in which Wanda Maximoff and Vision - two super-powered beings living their ideal suburban lives - begin to suspect that everything is not as it seems.',
         image: 'images/slider 3.PNG'
     },
     {
         name: 'raya and the last dragon',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        des: 'Raya and the Last Dragon travels to the fantasy world of Kumandra, where humans and dragons lived together in harmony long ago. But when an evil force threatened the land, the dragons sacrificed themselves to save humanity.',
         image: 'images/slider 4.PNG'
     },
     {
         name: 'luca',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        des: "A young boy experiences an unforgettable seaside summer on the Italian Riviera filled with gelato, pasta and endless scooter rides. Luca shares these adventures with his newfound best friend, but all the fun is threatened by a deeply-held secret: he is a sea monster from another world just below the ocean's surface.",
         image: 'images/slider 5.PNG'
     },
     {
         name: 'karthika deepam',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        des: 'The story revolves around the life of Deepa (Premi Viswanath), a dark-skinned and pure-hearten girl. She faces several trials and tribulations due to her dark-skin complexion. She gets married to a Doctor, Karthik (Nirupam Paritala) who becomes her faith of love and support in every conditions.',
         image: 'images/slider 7.jpg'
     },
 ]
