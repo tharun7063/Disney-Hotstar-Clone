40015219b5b262fb02890472e2ffcc0a8dcd8bf4
tharun7063 <tharundharavath34@gmail.com>
2023-11-10 16:40:03 +0530

    initial commit

---
+++ data.js
@@ -0,0 +1,32 @@
+let movies = [
+    {
+        name: 'daya',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 6.jpg'
+    },
+    {
+        name: 'loki',
+        des: ' sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 5.PNG'
+    },
+    {
+        name: 'karthika deepam',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 7.jpg'
+    },
+]
